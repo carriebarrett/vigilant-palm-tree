a325d558969d3743a4bb52b77342a5a51f065048
carriebarrett <67287344+carriebarrett@users.noreply.github.com>
2021-09-21 19:25:01 -0700

    updated design of home screen and launch screen

--- Financial Mindset Affirmations/ViewController.swift
+++ Financial Mindset Affirmations/ViewController.swift
@@ -21,16 +21,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // code to update titles w/current affirmations goes here
-        var number1 = Int.random(in: 0...4)
-        var number2 = Int.random(in: 0...4)
+        // code to randomize titles w/current affirmations goes here
+        var number1 = Int.random(in: 0..<affirmationMessages.count)
+        var number2 = Int.random(in: 0..<affirmationMessages.count)
         while number2 == number1 {
-            number2 = Int.random(in: 0...4)
+            number2 = Int.random(in: 0..<affirmationMessages.count)
         }
-        var number3 = Int.random(in: 0...4)
+        var number3 = Int.random(in: 0..<affirmationMessages.count)
         while number3 == number1 || number3 == number2 {
-            number3 = Int.random(in: 0...4)
+            number3 = Int.random(in: 0..<affirmationMessages.count)
         }
+        
         affirmationA.setTitle(affirmationMessages[number1].text, for: .normal)
         affirmationB.setTitle(affirmationMessages[number2].text, for: .normal)
         affirmationC.setTitle(affirmationMessages[number3].text, for: .normal)
@@ -43,8 +44,8 @@ class ViewController: UIViewController {
         
         // notification content
         let content = UNMutableNotificationContent()
-        content.title = "Hey I'm a notification"
-        content.body = "Look at me!"
+        content.title = "Today's affirmation"
+        content.body = affirmationMessages[number1].text
         
         // notification trigger
         let date = Date().addingTimeInterval(10)
@@ -68,6 +69,7 @@ class ViewController: UIViewController {
 
 }
 
+// categories of affirmations (type-safe)
 enum Category {
     case moneyMindset
     case income
@@ -75,15 +77,23 @@ enum Category {
     case other
 }
 
+// data structure for the affirmations
 struct Affirmation {
     var text = ""
     var category = Category.other
 }
 
+// list of affirmations
+// (if list gets longer or changes then using a database would be better)
 var affirmationMessages =
     [Affirmation(text: "I deserve a prosperous life", category: Category.moneyMindset),
      Affirmation(text: "Money is a tool that can change my life for the better", category: Category.moneyMindset),
      Affirmation(text: "I control money, money doesn't control me", category: Category.moneyMindset),
      Affirmation(text: "Money flows to me freely as I move through this world", category: Category.moneyMindset),
-     Affirmation(text: "I can use money to create a better life", category: Category.moneyMindset)
+     Affirmation(text: "I can use money to create a better life", category: Category.moneyMindset),
+     Affirmation(text: "My finances don't scare me because I have a plan", category: Category.moneyMindset),
+     Affirmation(text: "I am worthy of a solid financial foundation", category: Category.moneyMindset),
+     Affirmation(text: "Money can expand the opportunities of my life", category: Category.moneyMindset),
+     Affirmation(text: "Negative emotions about money don't serve my financial goals", category: Category.moneyMindset),
+     Affirmation(text: "I am a very capable person that can tackle all money obstacles", category: Category.moneyMindset)
     ]
